0f8b8a09f07480e6a2613f54edfcd13376a4e520
Thang Nguyen <jessepinkman97@gmail.com>
2025-06-02 14:27:19 +0700

    Sort categories for header navigation

--- src/components/header/HeaderNavigation.tsx
+++ src/components/header/HeaderNavigation.tsx
@@ -9,17 +9,24 @@ import Link from "next/link";
 export default function HeaderNavigation() {
   const { categories } = useCategories()
   const level1Categories = categories.filter((category) => category.level === 1)
-  function getChildCategories(searchId: number,className?:string) {
-    const result = categories.filter((category) => category.parentId === searchId)
+  function getChildCategories(searchId: number, className?: string) {
+    const result = categories
+      .filter(category => category.parentId === searchId)
+      .map(category => ({
+        ...category,
+        childrenCount: categories.filter(c => c.parentId === category.id).length
+      }))
+      .sort((a, b) => b.childrenCount - a.childrenCount);
+
     return (
       result.map((category) => (
-        <div key={category.id} className={cn("",className)}>
-          <ul className="flex flex-col min-w-[400px] mt-4"> 
+        <div key={category.id} className={cn("", className)}>
+          <ul className="flex flex-col min-w-[400px] mt-4">
             <li className="cursor-pointer ">
               <Link className="hover:underline hover:opacity-80" href={category?.path ?? ""}>{category.name}</Link>
               <ul>
                 {
-                  getChildCategories(category.id,"font-normal my-2 gap-4 text-base hover:underline ")
+                  getChildCategories(category.id, "font-normal my-2 gap-4 text-base hover:underline ")
                 }
               </ul>
             </li>
@@ -31,25 +38,27 @@ export default function HeaderNavigation() {
   return (
     <NavigationMenu className="hidden md:block">
       <NavigationMenuList>
-       {
-        level1Categories.map((grandCategory) => (
-          <NavigationMenuItem key={grandCategory.id}>
-            <NavigationMenuTrigger className="bg-transparent hover:bg-none font-semibold text-base">
-              <Link href={grandCategory?.path ? grandCategory?.path : ""}>{grandCategory.name}</Link>
-            </NavigationMenuTrigger>
-            <NavigationMenuContent className="bg-brand-light/30 border-t-none shadow-none">
-              <NavigationMenuLink asChild>
-              <ul className=" gap-3 grid min-w-[700px] grid-cols-3 p-6">
-                {
-                  getChildCategories(grandCategory.id,"font-semibold text-lg")
-                }
-              </ul>
+        {
+          level1Categories.map((grandCategory) => {
+            return (
+              <NavigationMenuItem key={grandCategory.id}>
+                <NavigationMenuTrigger className="bg-transparent hover:bg-none font-semibold text-base">
+                  <Link href={grandCategory?.path ? grandCategory?.path : ""}>{grandCategory.name}</Link>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="bg-brand-light/30 border-t-none shadow-none">
+                  <NavigationMenuLink asChild>
+                    <ul className=" gap-3 grid min-w-[700px] grid-cols-3 p-6">
+                      {
+                        getChildCategories(grandCategory.id, "font-semibold text-lg")
+                      }
+                    </ul>
 
-              </NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        ))
-       }
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            )
+          })
+        }
       </NavigationMenuList>
     </NavigationMenu>
   )
