884b48a3d44b6c8fd9ef8dc0e703f390239464d2
Thang Nguyen <jessepinkman97@gmail.com>
2025-07-07 12:36:55 +0700

    Fix spinner at loading bar

--- src/components/provider/ProgressProvider.tsx
+++ src/components/provider/ProgressProvider.tsx
@@ -5,6 +5,7 @@ import { AppProgressProvider as ProgressProvider } from '@bprogress/next';
 export default function ProgressProviderComponent({ children }: { children: React.ReactNode }) {
     return <ProgressProvider
     color="#FBDB93"
+    options={{ showSpinner: false }}
     height='3px'
     shallowRouting
 >{children}</ProgressProvider>;
